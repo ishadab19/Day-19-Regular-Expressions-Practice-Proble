920a35671d9b263321e8cdc5b3d34adbcb13d1e3
shadab <me.shadab19@gmail.com>
2022-10-26 22:37:55 +0530

    [Added - First Name Validation]

--- src/com/Regex/java/userRegistration.java
+++ src/com/Regex/java/userRegistration.java
@@ -2,8 +2,19 @@ package com.Regex.java;
 import java.util.*;
 import java.util.regex.Pattern;
 public class userRegistration {
+	
+	public boolean userName(String regex , String input) {
+		return Pattern.compile(regex).matcher(input).matches();
+	}
+
 	public static void main(String[] args) {
-		System.out.println("welcome To Regex Problem");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name :");
+		String firstName = "^[A-Z]{1}[a-z]{2,}$";
+		String input = sc.next(); //Shadab = true // shadab = false //
+		userRegistration ur = new userRegistration();
+		System.out.println(ur.userName(firstName, input));
+
 	}
 
 }
